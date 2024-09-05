d2678006e9f814825ce65b072cafff9bfcf3c303
Mingkuan Yan <86630468+MingkuanY@users.noreply.github.com>
2024-09-05 16:57:29 -0400

    Speech recognition with Web Speech API!!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,58 @@ import Icon from "@/components/Icon";
 import styles from "../styles/home.module.scss";
 import { useState } from "react";
 
+type Message = "DEFAULT" | "LISTENING" | "SUMMARY";
+
 export default function Home() {
-  const [message, setMessage] = useState(
-    "Tell me something you want to add to our story!"
-  );
+  const [message, setMessage] = useState<Message>("DEFAULT");
+  const [input, setInput] = useState<string>();
+  const [isRecording, setIsRecording] = useState(false);
+
+  const handleOnRecord = () => {
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+
+    if (!isRecording) {
+      // Start recording
+      setMessage("LISTENING");
+      recognition.start();
+      setIsRecording(true);
+
+      recognition.onresult = async (event) => {
+        const transcript = event.results[0][0].transcript;
+        setInput(transcript);
+        setMessage("SUMMARY");
+        setIsRecording(false);
+      };
+
+      recognition.onend = () => {
+        setMessage("SUMMARY");
+        setIsRecording(false);
+      };
+    } else {
+      // Stop recording
+      recognition.stop();
+      setMessage("SUMMARY");
+      setIsRecording(false);
+    }
+  };
 
   return (
     <div className={styles.main}>
       <div className={styles.generationContainer}>
-        <button className={styles.micBtn}>
+        <button className={styles.micBtn} onClick={handleOnRecord}>
           <Icon type="mic" fill="#FFF" />
         </button>
-        <p className={styles.text}>{message}</p>
+        {message === "DEFAULT" && (
+          <p className={styles.text}>
+            Tell me something you want to add to our story!
+          </p>
+        )}
+        {message === "LISTENING" && (
+          <p className={styles.text}>Go ahead, I'm listening...</p>
+        )}
+        {message === "SUMMARY" && <p className={styles.text}>{input}</p>}
       </div>
     </div>
   );

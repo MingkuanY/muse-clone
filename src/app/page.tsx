117f5d5a29d8d48af0609859b613224ff6155ee2
Mingkuan Yan <86630468+MingkuanY@users.noreply.github.com>
2024-09-05 20:09:00 -0400

    Generates full story with proper formatting and ease-in scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Icon from "@/components/Icon";
 import styles from "../styles/home.module.scss";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type Message = "DEFAULT" | "LISTENING" | "SUMMARY";
 
@@ -21,7 +21,6 @@ export default function Home() {
 
     recognition.onresult = async (event) => {
       const transcript = event.results[0][0].transcript;
-      console.log("transcript:", transcript);
       const response = await fetch("/api/story", {
         method: "POST",
         body: JSON.stringify({
@@ -29,7 +28,6 @@ export default function Home() {
           type: "summary",
         }),
       }).then((r) => r.json());
-      console.log("summary:", response.text);
       setSummary(response.text);
       setMessage("SUMMARY");
     };
@@ -37,32 +35,61 @@ export default function Home() {
     recognition.start();
   };
 
-  const generateContent = async (input: string, type: "summary" | "story") => {
+  const storyRef = useRef<HTMLDivElement>(null);
+
+  const handleOnGenerate = async () => {
     const response = await fetch("/api/story", {
       method: "POST",
-      body: JSON.stringify({ input, type }),
-    });
+      body: JSON.stringify({
+        input: summary,
+        type: "story",
+      }),
+    }).then((r) => r.json());
 
-    const data = await response.json();
-    return data.result;
+    const formattedStory = response.text.split(". ").join(".\n\n");
+
+    setStory(formattedStory);
   };
 
+  useEffect(() => {
+    if (story && storyRef.current) {
+      console.log("story: ", story);
+      storyRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  }, [story]);
+
   return (
-    <div className={styles.main}>
-      <div className={styles.generationContainer}>
-        <button className={styles.micBtn} onClick={handleOnRecord}>
-          <Icon type="mic" fill="#FFF" />
-        </button>
-        {message === "DEFAULT" && (
-          <p className={styles.text}>
-            Tell me something you want to add to our story!
-          </p>
-        )}
-        {message === "LISTENING" && (
-          <p className={styles.text}>Go ahead, I'm listening...</p>
-        )}
-        {message === "SUMMARY" && <p className={styles.text}>{summary}</p>}
+    <>
+      <div className={styles.main}>
+        <div className={styles.generationContainer}>
+          <button className={styles.micBtn} onClick={handleOnRecord}>
+            <Icon type="mic" fill="#FFF" />
+          </button>
+          {message === "DEFAULT" && (
+            <p className={styles.text}>
+              Tell me something you want to add to our story!
+            </p>
+          )}
+          {message === "LISTENING" && (
+            <p className={styles.text}>Go ahead, I'm listening...</p>
+          )}
+          {message === "SUMMARY" && <p className={styles.text}>{summary}</p>}
+          {message === "SUMMARY" && (
+            <button
+              className={styles.genFullStoryBtn}
+              onClick={handleOnGenerate}
+            >
+              Write this story!
+            </button>
+          )}
+        </div>
       </div>
-    </div>
+      <div className={styles.storyPage} ref={storyRef}>
+        {story}
+      </div>
+    </>
   );
 }
